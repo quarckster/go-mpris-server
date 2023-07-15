c20bf185e10aaf19819e4dc9420d38c1b6290c32
Drew Weymouth <dweymouth@gmail.com>
2023-07-15 10:44:04 -0700

    fix emitting Metadata PropertiesChanged signal

--- internal/common.go
+++ internal/common.go
@@ -4,6 +4,7 @@ import (
 	"reflect"
 
 	"github.com/godbus/dbus/v5"
+	"github.com/quarckster/go-mpris-server/pkg/types"
 )
 
 func makeError(err error) *dbus.Error {
@@ -27,10 +28,17 @@ func Changes(adapter interface{}, props []string) (map[string]dbus.Variant, erro
 	changes := map[string]dbus.Variant{}
 	for _, prop := range props {
 		reflectValues := reflect.ValueOf(adapter).MethodByName(prop).Call([]reflect.Value{})
-		variant := dbus.MakeVariant(reflectValues[0].Interface())
 		err, _ := reflectValues[1].Interface().(error)
 		if err != nil {
 			return map[string]dbus.Variant{}, err
+		}
+
+		// need to handle Metadata as a special case since it needs to be converted to map
+		var variant dbus.Variant
+		if meta, ok := reflectValues[0].Interface().(types.Metadata); ok {
+			variant = dbus.MakeVariant(meta.MakeMap())
+		} else {
+			variant = dbus.MakeVariant(reflectValues[0].Interface())
 		}
 		changes[prop] = variant
 	}
